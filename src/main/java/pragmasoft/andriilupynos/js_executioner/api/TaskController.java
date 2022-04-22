ff36b1f0162e593588ee8204f5028400ce541477
Curious-Eye <rs.lupynosa@gmail.com>
2022-04-22 19:17:23 +0300

    [ADD] Added HATEOAS endpoints links

--- src/main/java/pragmasoft/andriilupynos/js_executioner/api/TaskController.java
+++ src/main/java/pragmasoft/andriilupynos/js_executioner/api/TaskController.java
@@ -1,6 +1,9 @@
 package pragmasoft.andriilupynos.js_executioner.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,6 +19,12 @@ import pragmasoft.andriilupynos.js_executioner.service.TaskScheduleService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api")
 public class TaskController {
@@ -30,19 +39,23 @@ public class TaskController {
      */
     @PostMapping("/tasks")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public Mono<TaskCreateRespDto> createTask(@RequestBody TaskCreateRqDto rq) {
+    public Mono<EntityModel<TaskCreateRespDto>> createTask(@RequestBody TaskCreateRqDto rq) {
         return taskScheduleService.schedule(
-                TaskScheduleService.TaskScheduleModel.builder()
-                        .code(rq.getCode())
-                        .name(rq.getName())
-                        .executionDate(rq.getExecutionDate())
-                        .build()
-        )
-                .map(task ->
-                    TaskCreateRespDto.builder()
-                            .task(toTaskDto(task))
-                            .build()
+                        TaskScheduleService.TaskScheduleModel.builder()
+                                .code(rq.getCode())
+                                .name(rq.getName())
+                                .executionDate(rq.getExecutionDate())
+                                .build()
                 )
+                .map(task -> {
+                    var taskDto = toTaskDto(task);
+                    return EntityModel.of(
+                            TaskCreateRespDto.builder()
+                                    .task(taskDto)
+                                    .build(),
+                            getTaskHateoasLinks(taskDto)
+                    );
+                })
                 .onErrorResume(err -> Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, err.getMessage())));
     }
 
@@ -50,9 +63,10 @@ public class TaskController {
      * Find task by id, including current execution output
      */
     @GetMapping("/tasks/{id}")
-    public Mono<TaskDto> findTaskById(@PathVariable String id) {
+    public Mono<EntityModel<TaskDto>> findTaskById(@PathVariable String id) {
         return taskQueryService.getTaskWithCurrentOutputById(id)
                 .map(this::toTaskDto)
+                .map(taskDto -> EntityModel.of(taskDto, getTaskHateoasLinks(taskDto)))
                 .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)));
     }
 
@@ -61,21 +75,23 @@ public class TaskController {
      * Optionally filter by status or sort be createdDate
      */
     @GetMapping("/tasks")
-    public Flux<TaskDto> findTasks(
+    public Flux<EntityModel<TaskDto>> findTasks(
             @RequestParam(required = false) TaskStatusDto status,
             @RequestParam(required = false) Boolean newFirst
     ) {
         return taskQueryService.getTasksMatching(status, newFirst)
-                .map(this::toTaskDto);
+                .map(this::toTaskDto)
+                .map(taskDto -> EntityModel.of(taskDto, getTaskHateoasLinks(taskDto)));
     }
 
     /**
      * Find task by name
      */
     @GetMapping("/tasks/actions/find-by-name")
-    public Mono<TaskDto> findTaskByName(@RequestParam String name) {
+    public Mono<EntityModel<TaskDto>> findTaskByName(@RequestParam String name) {
         return taskStore.findByName(name)
                 .map(this::toTaskDto)
+                .map(taskDto -> EntityModel.of(taskDto, getTaskHateoasLinks(taskDto)))
                 .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)));
     }
 
@@ -83,8 +99,15 @@ public class TaskController {
      * Stop task's execution
      */
     @PutMapping("/tasks/{id}/stop-execution")
-    public Mono<Void> stopTask(@PathVariable String id) {
-        return taskExecuteService.stopById(id);
+    public Mono<RepresentationModel> stopTask(@PathVariable String id) {
+        return taskExecuteService.stopById(id)
+                .thenReturn(RepresentationModel.of(
+                        null,
+                        List.of(
+                                linkTo(methodOn(TaskController.class).findTasks(null, null))
+                                        .withRel("tasks")
+                        )
+                ));
     }
 
     private TaskDto toTaskDto(Task task) {
@@ -100,6 +123,15 @@ public class TaskController {
                 .endExecDate(task.getEndExecDate())
                 .createdDate(task.getCreatedDate())
                 .build();
+    }
+
+    private List<Link> getTaskHateoasLinks(TaskDto taskDto) {
+        var links = new ArrayList<Link>();
+        links.add(linkTo(methodOn(TaskController.class).findTaskById(taskDto.getId())).withSelfRel());
+        links.add(linkTo(methodOn(TaskController.class).findTasks(null, null)).withRel("tasks"));
+        if (taskDto.getStatus() == TaskStatusDto.EXECUTING)
+            links.add(linkTo(methodOn(TaskController.class).stopTask(taskDto.getId())).withRel("stop-task"));
+        return links;
     }
 
 }
